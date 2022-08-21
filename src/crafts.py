fccf88f1eb9a3fde81772d346c7ffbcaa0196e72
Fe3O4-Git <Fe3O4_Live@Outlook.com>
2022-08-21 18:11:52 +0800

    optimize crafts generator

--- src/crafts.py
+++ src/crafts.py
@@ -34,6 +34,12 @@ def get_item_id(item: str | dict) -> str:
 def replace_item(cmd: str, item: dict) -> str:
     cmd = cmd.replace("%%ID%%", get_item_id(item))
     cmd = cmd.replace("%%COUNT%%", get_item_count(item))
+    if "mod_id" in item:
+        cmd = cmd.replace("%%MOD_ID%%", item["mod_id"])
+    if "name" in item:
+        cmd = cmd.replace("%%NAME%%", utils.gen_json(item["name"]))
+    if "description" in item:
+        cmd = cmd.replace("%%LORE%%", utils.gen_json(item["description"]))
     return cmd
 
 
@@ -85,9 +91,6 @@ def cmd_detect_others(material: dict) -> str:
 def get_craft_cmd_gen_item(product: dict) -> str:
     cmd = statics.craft_bases["gen_item"]
     cmd = replace_item(cmd, product)
-    cmd = cmd.replace("%%MOD_ID%%", product["mod_id"])
-    cmd = cmd.replace("%%NAME%%", utils.gen_json(product["name"]))
-    cmd = cmd.replace("%%LORE%%", utils.gen_json(product["description"]))
     return cmd
 
 
@@ -98,43 +101,53 @@ def cmd_detect_block(block: dict) -> str:
     return cmd
 
 
+def write_craft_file(project_name: str, craft_file: str, receipe: dict) -> None:
+    with open(craft_file, "w") as fs:
+        material_or_s = receipe["material"]
+        first_material = utils.sfirst(material_or_s)
+        fs.write(cmd_detect_main(project_name, receipe["name"], first_material))
+        if "block" in receipe:
+
+            def func(block: dict) -> None:
+                fs.write(cmd_detect_block(block))
+
+            utils.smap(func, receipe["block"])
+        if utils.is_list_like(material_or_s):
+            for material in material_or_s[1:]:
+                fs.write(cmd_detect_others(material))
+        fs.write(cmd_detect_tag(project_name, receipe["name"]))
+        if utils.is_list_like(material_or_s):
+            for material in material_or_s[1:]:
+                fs.write(cmd_clear_others(project_name, receipe["name"], material))
+        fs.write(get_craft_cmd_run_func(project_name, receipe["name"]))
+        fs.write(get_craft_cmd_clear_self(project_name, receipe["name"]))
+
+
+def write_item_file(item_file: str, receipe: dict) -> None:
+    with open(item_file, "w") as fs:
+
+        def func(product: dict) -> None:
+            fs.write(get_craft_cmd_gen_item(product))
+
+        utils.smap(func, receipe["product"])
+
+
 def gen(tmp_dir: str, receipes: list) -> None:
     project_name = os.path.basename(os.path.dirname(tmp_dir))
     data_dir = utils.join_path(tmp_dir, "data", project_name)
     func_dir = utils.join_path(data_dir, "functions")
-    func_ext = ".mcfunction"
     func_tags_dir = utils.join_path(data_dir, "tags", "functions")
+    func_ext = ".mcfunction"
+    craft_func_file = utils.join_path(func_dir, "craft" + func_ext)
     crafts_dir = utils.join_path(func_dir, "crafts")
     items_dir = utils.join_path(func_dir, "items")
     utils.smap(utils.smkdir, (func_tags_dir, crafts_dir, items_dir))
-    func_tag_tick = []
     for receipe in receipes:
-        craft_name = receipe["name"]
-        craft_file = utils.join_path(crafts_dir, craft_name + func_ext)
-        item_file = utils.join_path(items_dir, craft_name + func_ext)
-        with open(craft_file, "w") as fs_craft:
-            material_or_s = receipe["material"]
-            material = utils.sfirst(material_or_s)
-            fs_craft.write(cmd_detect_main(project_name, craft_name, material))
-            if "block" in receipe:
-                fs_craft.write(cmd_detect_block(receipe["block"]))
-            if utils.is_list_like(material_or_s):
-                for per_material in material_or_s[1:]:
-                    fs_craft.write(cmd_detect_others(per_material))
-            fs_craft.write(cmd_detect_tag(project_name, craft_name))
-            if utils.is_list_like(material_or_s):
-                for per_material in material_or_s[1:]:
-                    fs_craft.write(cmd_clear_others(project_name, craft_name, per_material))
-            fs_craft.write(get_craft_cmd_run_func(project_name, craft_name))
-            fs_craft.write(get_craft_cmd_clear_self(project_name, craft_name))
-        with open(item_file, "w") as fs_item:
-
-            def func(product):
-                fs_item.write(get_craft_cmd_gen_item(product))
-
-            utils.smap(func, receipe["product"])
-        func_tag_tick.append("%s:%s" % (project_name, craft_name))
-        if not utils.is_list_like(func_tag_tick):
-            func_tag_tick = func_tag_tick[0]
-        with open(utils.join_path(func_tags_dir, "tick.json"), "w") as fs:
-            fs.write(utils.gen_json({"values": func_tag_tick}))
+        craft_file = utils.join_path(crafts_dir, receipe["name"] + func_ext)
+        item_file = utils.join_path(items_dir, receipe["name"] + func_ext)
+        write_craft_file(project_name, craft_file, receipe)
+        write_item_file(item_file, receipe)
+        with open(craft_func_file, "a") as fs:
+            fs.write("function %s:crafts/%s" % (project_name, receipe["name"]))
+    with open(utils.join_path(func_tags_dir, "tick.json"), "w") as fs:
+        fs.write(utils.gen_json({"values": "%s:crafts" % (project_name)}, True))
